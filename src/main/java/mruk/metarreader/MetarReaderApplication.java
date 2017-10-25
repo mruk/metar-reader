20d4d9dfb69047bf9c31df6d66abd79cbe562c31
Konrad <667358+mruk@users.noreply.github.com>
2017-10-25 15:42:51 +0200

    Create MetarReaderApplication.java

---
+++ src/main/java/mruk/metarreader/MetarReaderApplication.java
@@ -0,0 +1,11 @@
+package mruk.metarreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplicationpublic
+class MetarReaderApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MetarReaderApplication.class, args);
+  }
+}
